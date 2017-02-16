9bbba80e2b509795da3e00acbd912a2e209b55d3
Thomas Withers <tomtucka@gmail.com>
2017-02-16 22:43:40 +0000

    Removed some plugins, changed database and made footer sticky

--- wp-content/themes/gough/footer.php
+++ wp-content/themes/gough/footer.php
@@ -4,10 +4,10 @@
 		<div class="row">
 			<div class="col-sm-6">
 				<ul class="social-links">
-					<li><a href="#link"><i class="fa fa-twitter fa-fw"></i></a></li>
-					<li><a href="#link"><i class="fa fa-github fa-fw"></i></a></li>
-					<li><a href="#link"><i class="fa fa-stack-overflow fa-fw"></i></a></li>
-					<li><a href="#link"><i class="fa fa-linkedin fa-fw"></i></a></li>
+					<li><a href="https://twitter.com/tomtucka"><i class="fa fa-twitter fa-fw"></i></a></li>
+					<li><a href="https://github.com/GuruTom"><i class="fa fa-github fa-fw"></i></a></li>
+					<li><a href="http://stackoverflow.com/users/2993494/gurutom"><i class="fa fa-stack-overflow fa-fw"></i></a></li>
+					<li><a href="https://www.linkedin.com/in/tomtucka/"><i class="fa fa-linkedin fa-fw"></i></a></li>
 				</ul>
 			</div>
 			<div class="col-sm-6 text-right">
